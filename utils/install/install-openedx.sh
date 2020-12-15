3d9de38fe15b422e9619d62d5eecd319ed26b9c4
johnpatrick23 <johnpatrickpapares@gmail.com>
2020-12-15 15:51:12 +0800

    Revert to base

--- utils/install/install-openedx.sh
+++ utils/install/install-openedx.sh
@@ -1,83 +1,16 @@
 #!/bin/bash
 
-EDX_CONFIGURATION_PUBLIC_GITHUB_ACCOUNTNAME=""
-EDX_CONFIGURATION_PUBLIC_GITHUB_PROJECTNAME=""
-EDX_CONFIGURATION_PUBLIC_GITHUB_PROJECTBRANCH=""
-EDXAPP_LMS_BASE=""
-EDXAPP_CMS_BASE=""
-
-help()
-{
-    echo "This script sets up SSH, installs MDSD and runs the DB bootstrap"
-    echo "Options:"
-    echo "        --edxconfiguration-public-github-accountname Name of the account that owns the edx configuration repository"
-    echo "        --edxconfiguration-public-github-projectname Name of the edx configuration GitHub repository"
-    echo "        --edxconfiguration-public-github-projectbranch Branch of edx configuration GitHub repository"
-    echo "        --edxplatform-lms-base Platform base URL for LMS"
-    echo "        --edxplatform-cms-base Platform base URL for CMS"
-}
-
-# Parse script parameters
-parse_args()
-{
-    while [[ "$#" -gt 0 ]]
-    do
-
-        arg_value="${2}"
-        shift_once=0
-
-        if [[ "${arg_value}" =~ "--" ]]; 
-        then
-            arg_value=""
-            shift_once=1
-        fi
-
-         # Log input parameters to facilitate troubleshooting
-        echo "Option '${1}' set with value '"${arg_value}"'"
-
-        case "$1" in
-            --edxconfiguration-public-github-accountname)
-                EDX_CONFIGURATION_PUBLIC_GITHUB_ACCOUNTNAME="${arg_value}"
-                ;;
-            --edxconfiguration-public-github-projectname)
-                EDX_CONFIGURATION_PUBLIC_GITHUB_PROJECTNAME="${arg_value}"
-                ;;
-            --edxconfiguration-public-github-projectbranch)
-                EDX_CONFIGURATION_PUBLIC_GITHUB_PROJECTBRANCH="${arg_value}"
-                ;;
-            --edxplatform-lms-base)
-                EDXAPP_LMS_BASE="${arg_value}"
-                ;;
-            --edxplatform-cms-base)
-                EDXAPP_CMS_BASE="${arg_value}"
-                ;;
-            -h|--help)  # Helpful hints
-                help
-                exit 2
-                ;;
-            *) # unknown option
-                echo "Option '${BOLD}$1${NORM} ${arg_value}' not allowed."
-                help
-                exit 2
-                ;;
-        esac
-
-        shift # past argument or value
-
-        if [ $shift_once -eq 0 ]; 
-        then
-            shift # past argument or value
-        fi
-    done
-}
-
-# parse script arguments
-parse_args $@
-
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
 sudo su
+
+export OPENEDX_RELEASE=open-release/ironwood.master
+export EDX_CONFIGURATION_PUBLIC_GITHUB_ACCOUNTNAME=edx
+export EDX_CONFIGURATION_PUBLIC_GITHUB_PROJECTNAME=configuration
+export EDXAPP_LMS_BASE=enialrash.org
+export EDXAPP_CMS_BASE=studio.enialrash.org
+
 
 locale-gen en_US en_US.UTF-8
 export LC_ALL="en_US.UTF-8"
@@ -87,6 +20,7 @@ cd ~
 echo "EDXAPP_LMS_BASE: \"$EDXAPP_LMS_BASE\"" >> config.yml
 echo "EDXAPP_CMS_BASE: \"$EDXAPP_CMS_BASE\"" >> config.yml
 
+
 apt-get install rabbitmq-server -y
 apt autoremove -y
 systemctl enable rabbitmq-server
@@ -95,14 +29,12 @@ rabbitmq-plugins enable rabbitmq_management
 
 apt-get install python3-pip -y
 
-export OPENEDX_RELEASE=$EDX_CONFIGURATION_PUBLIC_GITHUB_PROJECTBRANCH
-
-wget https://raw.githubusercontent.com/$EDX_CONFIGURATION_PUBLIC_GITHUB_ACCOUNTNAME/$EDX_CONFIGURATION_PUBLIC_GITHUB_PROJECTNAME/$EDX_CONFIGURATION_PUBLIC_GITHUB_PROJECTBRANCH/util/install/ansible-bootstrap.sh -O - | sudo -H bash
+wget https://raw.githubusercontent.com/edx/configuration/$OPENEDX_RELEASE/util/install/ansible-bootstrap.sh -O - | sudo -H bash
 
 mkdir /home/rabbitmq/
 touch /home/rabbitmq/.erlan.cookie
 chown -Rv rabbitmq:root /home/rabbitmq/.
 
-wget https://raw.githubusercontent.com/$EDX_CONFIGURATION_PUBLIC_GITHUB_ACCOUNTNAME/$EDX_CONFIGURATION_PUBLIC_GITHUB_PROJECTNAME/$EDX_CONFIGURATION_PUBLIC_GITHUB_PROJECTBRANCH/util/install/generate-passwords.sh -O - | bash
+wget https://raw.githubusercontent.com/edx/configuration/$OPENEDX_RELEASE/util/install/generate-passwords.sh -O - | bash
 
-wget https://raw.githubusercontent.com/$EDX_CONFIGURATION_PUBLIC_GITHUB_ACCOUNTNAME/$EDX_CONFIGURATION_PUBLIC_GITHUB_PROJECTNAME/$EDX_CONFIGURATION_PUBLIC_GITHUB_PROJECTBRANCH/util/install/native.sh -O - | bash > install.out &
+wget https://raw.githubusercontent.com/edx/configuration/$OPENEDX_RELEASE/util/install/native.sh -O - | bash > install.out & 
