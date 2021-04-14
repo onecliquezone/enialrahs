3628256b37927850fcb69c1add452fdef39a678f
johnpatrick23 <johnpatrickpapares@gmail.com>
2021-04-14 13:31:36 +0800

    Update for KOA

--- utils/install/install-openedx.sh
+++ utils/install/install-openedx.sh
@@ -79,29 +79,14 @@ sudo apt-get upgrade -y
 
 sudo su
 
-locale-gen en_US en_US.UTF-8
-export LC_ALL="en_US.UTF-8"
-export LC_CTYPE="en_US.UTF-8"
 cd ~
 
 echo "EDXAPP_LMS_BASE: \"$EDXAPP_LMS_BASE\"" >> config.yml
 echo "EDXAPP_CMS_BASE: \"$EDXAPP_CMS_BASE\"" >> config.yml
 
-apt-get install rabbitmq-server -y
-apt autoremove -y
-systemctl enable rabbitmq-server
-systemctl start rabbitmq-server
-rabbitmq-plugins enable rabbitmq_management
-
-apt-get install python3-pip -y
-
 export OPENEDX_RELEASE=$EDX_CONFIGURATION_PUBLIC_GITHUB_PROJECTBRANCH
 
-wget https://raw.githubusercontent.com/$EDX_CONFIGURATION_PUBLIC_GITHUB_ACCOUNTNAME/$EDX_CONFIGURATION_PUBLIC_GITHUB_PROJECTNAME/$EDX_CONFIGURATION_PUBLIC_GITHUB_PROJECTBRANCH/util/install/ansible-bootstrap.sh -O - | sudo -H bash
-
-mkdir /home/rabbitmq/
-touch /home/rabbitmq/.erlan.cookie
-chown -Rv rabbitmq:root /home/rabbitmq/.
+wget https://raw.githubusercontent.com/$EDX_CONFIGURATION_PUBLIC_GITHUB_ACCOUNTNAME/$EDX_CONFIGURATION_PUBLIC_GITHUB_PROJECTNAME/$EDX_CONFIGURATION_PUBLIC_GITHUB_PROJECTBRANCH/util/install/ansible-bootstrap.sh -O - | sudo -E bash
 
 wget https://raw.githubusercontent.com/$EDX_CONFIGURATION_PUBLIC_GITHUB_ACCOUNTNAME/$EDX_CONFIGURATION_PUBLIC_GITHUB_PROJECTNAME/$EDX_CONFIGURATION_PUBLIC_GITHUB_PROJECTBRANCH/util/install/generate-passwords.sh -O - | bash
 
